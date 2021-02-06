cfbbb975cc577d1d61fe465fee0e06810f836277
Austin Leung <austinle@andrew.cmu.edu>
2021-02-06 18:20:06 -0500

    Chore model test and implementation

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,4 +2,21 @@ class Chore < ApplicationRecord
 
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :by_task, -> { joins(:task).order('tasks.name') }
+    scope :chronological, -> { order('due_on, completed') }
+    scope :done, -> { where('completed': true) }
+    scope :pending, -> { where('completed': false) }
+    scope :upcoming, -> { where('? <= due_on', Date.today) }
+    scope :past, -> { where('? > due_on', Date.today) }
+
+    def status
+        if (completed)
+            "Completed"
+        else
+            "Pending"
+        end
+    end
 end
